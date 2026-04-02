644f4727b2e2e3e81b7199aa55cebfcd4013e4d3
Арсений Аникин <ars.anikin.2003@gmail.com>
2026-04-03 00:23:29 +0400

    add GPU calculation

--- VectorSum/main.cu
+++ VectorSum/main.cu
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <time.h>
 
-#define BLOCK_SIZE 16
+#define BLOCK_SIZE 64
 
 long int vector_sum_cpu(const long int* A, int N) {
     long int sum = 0;
@@ -12,8 +12,27 @@ long int vector_sum_cpu(const long int* A, int N) {
     return sum;
 }
 
-__global__ void vector_sum_gpu(const long int* A, int N) {
-    
+__global__ void vector_sum_gpu(const long int* A, long int* res, int N) {
+    __shared__ long int block_data[BLOCK_SIZE];
+
+    int thread_id = threadIdx.x;
+    int global_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(global_id < N)
+        block_data[thread_id] = A[global_id];
+    __syncthreads();
+
+    int part = blockDim.x / 2;
+    while (part > 0) {
+        if (thread_id < part)
+            block_data[thread_id] += block_data[thread_id + part];
+
+        __syncthreads();
+        part = part / 2;
+    }
+
+    if (thread_id == 0)
+        res[blockIdx.x] = block_data[0];
 }
 
 void run_vector_sum_time_test(int N) {
@@ -22,7 +41,8 @@ void run_vector_sum_time_test(int N) {
     size_t bytes = (size_t)N * sizeof(long int);
 
     // CPU calculation
-    long int* cpu_vect = (long int*)malloc(bytes);
+    long int* cpu_vect;
+    cudaMallocHost(&cpu_vect, bytes);
 
     for (int i = 0; i < N; i++)
         cpu_vect[i] = 1;
@@ -35,114 +55,112 @@ void run_vector_sum_time_test(int N) {
 
     
     // GPU calculation
-    // float* gpu_result_C = (float*)malloc(bytes);
+    int max_grid = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    long int* gpu_vect;
+    long int* gpu_buf;
+    cudaMalloc(&gpu_vect, bytes);
+    cudaMalloc(&gpu_buf, max_grid * sizeof(long int));
 
-    // float *gpu_A, *gpu_B, *gpu_C;
-    // cudaMalloc(&gpu_A, bytes);
-    // cudaMalloc(&gpu_B, bytes);
-    // cudaMalloc(&gpu_C, bytes);
-
-    // cudaEvent_t gpu_start_time, gpu_end_time;
-    // cudaEventCreate(&gpu_start_time);
-    // cudaEventCreate(&gpu_end_time);
-
-
-    // // copy array to the gpu
-    // cudaEventRecord(gpu_start_time);
-
-    // cudaMemcpy(gpu_A, cpu_A, bytes, cudaMemcpyHostToDevice);
-    // cudaMemcpy(gpu_B, cpu_B, bytes, cudaMemcpyHostToDevice);
-
-    // cudaEventRecord(gpu_end_time);
-    // cudaEventSynchronize(gpu_end_time);
-
-    // float gpu_copy_values_time = 0.0;
-    // cudaEventElapsedTime(&gpu_copy_values_time, gpu_start_time, gpu_end_time);
-    // printf("GPU copy values time: %fms\n", gpu_copy_values_time);
+    cudaEvent_t gpu_start_time, gpu_end_time;
+    cudaEventCreate(&gpu_start_time);
+    cudaEventCreate(&gpu_end_time);
 
 
-    // // calc in the kernel gpu
-    // cudaEventRecord(gpu_start_time);
+    // copy array to the gpu
+    cudaEventRecord(gpu_start_time);
 
-    // dim3 block(BLOCK_SIZE, BLOCK_SIZE);
-    // dim3 grid((N + block.x - 1) / block.x, (N + block.y - 1) / block.y);
-    // matmul_gpu<<<grid, block>>>(gpu_A, gpu_B, gpu_C, N);
-    // cudaDeviceSynchronize();
+    cudaMemcpy(gpu_vect, cpu_vect, bytes, cudaMemcpyHostToDevice);
 
-    // cudaEventRecord(gpu_end_time);
-    // cudaEventSynchronize(gpu_end_time);
+    cudaEventRecord(gpu_end_time);
+    cudaEventSynchronize(gpu_end_time);
 
-    // float gpu_calc_time = 0.0;
-    // cudaEventElapsedTime(&gpu_calc_time, gpu_start_time, gpu_end_time);
-    // printf("GPU calc time: %fms\n", gpu_calc_time);
+    float gpu_copy_values_time = 0.0;
+    cudaEventElapsedTime(&gpu_copy_values_time, gpu_start_time, gpu_end_time);
+    printf("GPU copy values time: %fms\n", gpu_copy_values_time);
 
 
-    // // copy result to the cpu
-    // cudaEventRecord(gpu_start_time);
-    // cudaMemcpy(gpu_result_C, gpu_C, bytes, cudaMemcpyDeviceToHost);
+    // calc in the kernel gpu
+    cudaEventRecord(gpu_start_time);
 
-    // cudaEventRecord(gpu_end_time);
-    // cudaEventSynchronize(gpu_end_time);
+    int cur_size = N;
+    int grid_size = (cur_size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    vector_sum_gpu<<<grid_size, BLOCK_SIZE>>>(gpu_vect, gpu_vect, cur_size);
+    cudaDeviceSynchronize();
+    cur_size = grid_size;
 
-    // float gpu_copy_result_time = 0.0;
-    // cudaEventElapsedTime(&gpu_copy_result_time, gpu_start_time, gpu_end_time);
-    // printf("GPU copy result time: %fms\n", gpu_copy_result_time);
+    while (cur_size > 1) {
+        grid_size = (cur_size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        vector_sum_gpu<<<grid_size, BLOCK_SIZE>>>(gpu_vect, gpu_vect, cur_size);
+        cudaDeviceSynchronize();
+        cur_size = grid_size;
+    }
 
-    // float gpu_full_time = gpu_copy_result_time + gpu_copy_values_time + gpu_calc_time;
-    // printf("GPU full time: %fms\n", gpu_full_time);
+    cudaEventRecord(gpu_end_time);
+    cudaEventSynchronize(gpu_end_time);
+
+    float gpu_calc_time = 0.0;
+    cudaEventElapsedTime(&gpu_calc_time, gpu_start_time, gpu_end_time);
+    printf("GPU calc time: %fms\n", gpu_calc_time);
 
 
-    // // validation of calcuations 
-    // bool is_valid_calc = validation_calc(cpu_C, gpu_result_C, N * N);
-    // printf("Calculation are right: %s\n", is_valid_calc ? "Yes" : "No");
+    // copy result to the cpu
+    cudaEventRecord(gpu_start_time);
+
+    long int gpu_sum = 0;
+    cudaMemcpy(&gpu_sum, gpu_vect, sizeof(long int), cudaMemcpyDeviceToHost);
+    printf("GPU vector sum: %ld\n", gpu_sum);
+
+    cudaEventRecord(gpu_end_time);
+    cudaEventSynchronize(gpu_end_time);
+
+    float gpu_copy_result_time = 0.0;
+    cudaEventElapsedTime(&gpu_copy_result_time, gpu_start_time, gpu_end_time);
+    printf("GPU copy result time: %fms\n", gpu_copy_result_time);
+
+    float gpu_full_time = gpu_copy_result_time + gpu_copy_values_time + gpu_calc_time;
+    printf("GPU full time: %fms\n", gpu_full_time);
 
 
-    // // show acceleration
-    // printf("Acceleration calc: %f\n", cpu_time / gpu_calc_time);
-    // printf("Acceleration in general: %f\n", cpu_time / gpu_full_time);
+    // show acceleration
+    printf("Acceleration calc: %f\n", cpu_time / gpu_calc_time);
+    printf("Acceleration in general: %f\n", cpu_time / gpu_full_time);
 
-    // cudaEventDestroy(gpu_start_time);
-    // cudaEventDestroy(gpu_end_time);
-    // cudaFree(gpu_A);
-    // cudaFree(gpu_B);
-    // cudaFree(gpu_C);
-
-    // free(gpu_result_C);
-    free(cpu_vect);
+    cudaEventDestroy(gpu_start_time);
+    cudaEventDestroy(gpu_end_time);
+    cudaFree(gpu_vect);
+    cudaFree(gpu_buf);
+    cudaFreeHost(cpu_vect);
 }
 
 int main() {
     long int sizes[] = {100000, 1000000, 10000000, 100000000, 1000000000};
 
     // cuda init
-    // cudaEvent_t init_start_time, init_end_time;
-    // cudaEventCreate(&init_start_time);
-    // cudaEventCreate(&init_end_time);
+    cudaEvent_t init_start_time, init_end_time;
+    cudaEventCreate(&init_start_time);
+    cudaEventCreate(&init_end_time);
 
-    // cudaEventRecord(init_start_time);
-    // float *start_A, *start_B, *start_C;
-    // cudaMalloc(&start_A, sizeof(float));
-    // cudaMalloc(&start_B, sizeof(float));
-    // cudaMalloc(&start_C, sizeof(float));
+    cudaEventRecord(init_start_time);
+    long int *start_vect;
+    cudaMalloc(&start_vect, sizeof(long int));
 
-    // dim3 block(1, 1);
-    // dim3 grid(1, 1);
-    // matmul_gpu<<<grid, block>>>(start_A, start_B, start_C, 1);
-    // cudaDeviceSynchronize();
+    dim3 block(1, 1);
+    dim3 grid(1, 1);
+    vector_sum_gpu<<<grid, block>>>(start_vect, start_vect, 1);
+    cudaDeviceSynchronize();
 
-    // cudaEventRecord(init_end_time);
-    // cudaEventSynchronize(init_end_time);
+    cudaEventRecord(init_end_time);
+    cudaEventSynchronize(init_end_time);
 
-    // float cuda_init_time = 0.0;
-    // cudaEventElapsedTime(&cuda_init_time, init_start_time, init_end_time);
-    // printf("CUDA init time: %fms\n", cuda_init_time);
+    float cuda_init_time = 0.0;
+    cudaEventElapsedTime(&cuda_init_time, init_start_time, init_end_time);
+    printf("CUDA init time: %fms\n", cuda_init_time);
 
-    // cudaEventDestroy(init_start_time);
-    // cudaEventDestroy(init_end_time);
+    cudaEventDestroy(init_start_time);
+    cudaEventDestroy(init_end_time);
 
-    // cudaFree(start_A);
-    // cudaFree(start_B);
-    // cudaFree(start_C);
+    cudaFree(start_vect);
+
 
     for (int i = 0; i < 5; i++) {
         run_vector_sum_time_test(sizes[i]);
